a63966afef34b02aa39b0ba3fe1e5d5d36abf6e1
alouilouai <louaialoui1993@gmail.com>
2025-03-16 00:42:05 +0100

    I missed that push having two weeks without PC

--- server/src/config/database.config.ts
+++ server/src/config/database.config.ts
@@ -1,10 +1,10 @@
 export default () => ({
   database: {
-    type: process.env.DB_TYPE, // Should be set in environment
-    host: process.env.DB_HOST, // Should be set in environment
+    type: process.env.DB_TYPE,
+    host: process.env.DB_HOST, 
     port: process.env.DB_PORT ? parseInt(process.env.DB_PORT, 10) : undefined, // Parse if set
-    username: process.env.DB_USERNAME, // Should be set in environment
-    password: process.env.DB_PASSWORD, // Should be set in environment
-    name: process.env.DB_DATABASE, // Should be set in environment
+    username: process.env.DB_USERNAME, 
+    password: process.env.DB_PASSWORD, 
+    name: process.env.DB_DATABASE, 
   },
 });
